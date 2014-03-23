811d1e28e2e8cd78daafa63386dcf758864dd98b
Paul Bennett (zen) <paulbennett67@hotmail.com>
2014-03-23 23:56:42 +1100

    Add missing package dependencies in bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+curl https://raw.github.com/creationix/nvm/master/install.sh | sh
+
+# Load nvm and install latest production node
+source $HOME/.nvm/nvm.sh
+nvm install v0.10.17
+nvm use v0.10.17
+
 npm install
 PORT=8080 node server.js
