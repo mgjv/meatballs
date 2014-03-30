8ce97cf08a5124934bfbe9ec85b89730cb8783b6
Paul Bennett (zen) <paulbennett67@hotmail.com>
2014-03-30 16:26:04 +1100

    Refactor to remove redundant function and tidy

--- server.js
+++ server.js
@@ -1,119 +1,98 @@
-//	Customization
+// Customization
+var defaultPort = 8080;
 
-//var appPort = process.env.PORT || 8080;
-
-// Librairies
-
+// Libraries
 var express = require('express'), app = express();
-var http = require('http')
-  , server = http.createServer(app)
-  , io = require('socket.io').listen(server);
+var http = require('http');
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
 
 
 var jade = require('jade');
-// var io = require('socket.io').listen(app);
 var pseudoArray = ['admin']; //block the admin username (you can disable it)
 
 // Views Options
-
-app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.set("view options", { layout: false })
 
 app.configure(function() {
-	app.use(express.static(__dirname + '/public'));
+    app.use(express.static(__dirname + '/public'));
 });
 
-app.set('port', process.env.PORT || 8080);
+app.set('port', process.env.PORT || defaultPort);
 
-server.listen(app.get('port'), function () {
-    console.log("Server listening on port %d", // in %s mode",
-        app.get('port')
-        //,server.settings.env
-    );
-});
 io.set('log level', 1);
+server.listen(app.get('port'), function () {
+    console.log("Server listening on port %d", app.get('port'));
+});
 
 // Render and send the main page
-
 app.get('/', function(req, res){
-  res.render('home.jade');
+    res.render('home.jade');
 });
 
 // Handle the socket.io connections
-
 var users = 0; //count the users
 
 io.sockets.on('connection', function (socket) { // First connection
-	users += 1; // Add 1 to the count
-	reloadUsers(); // Send the count to all the users
+    users += 1;
+    reloadUsers(); // Send the count to all the users
 
-    // Test function that socket can send message to itself
+    // Test that socket can send message to itself
     socket.on('echo', function(msg) {
         socket.emit('echo', msg);
     });
 
-	socket.on('message', function (data) { // Broadcast the message to all
-		if(pseudoSet(socket))
-		{
-			var transmit = {date : new Date().toISOString(), pseudo : returnPseudo(socket), message : data};
-			socket.broadcast.emit('message', transmit);
-			console.log("user "+ transmit['pseudo'] +" said \""+data+"\"");
-		}
-	});
+    // Broadcast message to all
+    socket.on('message', function (data) {
+        var name = returnPseudo(socket);
+        if (name) {
+            var transmit = {date : new Date().toISOString(), pseudo : name, message : data};
+            socket.broadcast.emit('message', transmit);
+            console.log("user "+ transmit['pseudo'] +" said \""+data+"\"");
+        }
+    });
 
-	socket.on('setPseudo', function (data) { // Assign a name to the user
-		if (pseudoArray.indexOf(data) == -1) // Test if the name is already taken
-		{
-			socket.set('pseudo', data, function(){
-				pseudoArray.push(data);
-				socket.emit('pseudoStatus', 'ok');
-				console.log("user " + data + " connected");
-			});
-		}
-		else
-		{
-			socket.emit('pseudoStatus', 'error') // Send the error
-		}
-	});
+    socket.on('setPseudo', function (data) { // Assign a name to the user
+        if (pseudoArray.indexOf(data) == -1) { // Test if the name is already taken
+            socket.set('pseudo', data, function(){
+                pseudoArray.push(data);
+                socket.emit('pseudoStatus', 'ok');
+                console.log("user " + data + " connected");
+            });
+        } else {
+            socket.emit('pseudoStatus', 'error') // Send the error
+        }
+    });
 
-	socket.on('disconnect', function () { // Disconnection of the client
-		users -= 1;
-		reloadUsers();
-		if (pseudoSet(socket))
-		{
-			var pseudo;
-			socket.get('pseudo', function(err, name) {
-				pseudo = name;
-			});
-			var index = pseudoArray.indexOf(pseudo);
-			pseudo.slice(index - 1, 1);
-		}
-	});
+    socket.on('disconnect', function () { // Disconnection of the client
+        users -= 1;
+        reloadUsers();
+        if (returnPseudo(socket)) {
+            var pseudo;
+            socket.get('pseudo', function(err, name) {
+                pseudo = name;
+            });
+            var index = pseudoArray.indexOf(pseudo);
+            pseudo.slice(index - 1, 1);
+        }
+    });
 });
 
 function reloadUsers() { // Send the count of the users to all
-  if (users == 1) {
-	io.sockets.emit('nbUsers', {"nb": "is " + users});
-  } else {
-	io.sockets.emit('nbUsers', {"nb": "are " + users});
-  }
-}
-function pseudoSet(socket) { // Test if the user has a name
-	var test;
-	socket.get('pseudo', function(err, name) {
-		if (name == null ) test = false;
-		else test = true;
-	});
-	return test;
+    if (users == 1) {
+        io.sockets.emit('nbUsers', {"nb": "is " + users});
+    } else {
+        io.sockets.emit('nbUsers', {"nb": "are " + users});
+    }
 }
 function returnPseudo(socket) { // Return the name of the user
-	var pseudo;
-	socket.get('pseudo', function(err, name) {
-		if (name == null ) pseudo = false;
-		else pseudo = name;
-	});
-	return pseudo;
+    var pseudo;
+    socket.get('pseudo', function(err, name) {
+        if (name == null ) pseudo = false;
+        else pseudo = name;
+    });
+    return pseudo;
 }
 exports.server = server;
 exports.app = app;
