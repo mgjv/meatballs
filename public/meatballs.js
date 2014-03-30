cf35a5571dd2bf7c77e5b5c72ff77485bc157f34
Paul Bennett (zen) <paulbennett67@hotmail.com>
2014-03-30 16:25:01 +1100

    Bind Enter key to Send button

--- public/meatballs.js
+++ public/meatballs.js
@@ -3,88 +3,100 @@ var pseudo = "";
 
 // Init
 $(function() {
-	messageContainer = $('#messageInput');
-	submitButton = $("#submit");
-	bindButton();
-	window.setInterval(time, 1000*10);
-	$("#alertPseudo").hide();
-	$('#modalPseudo').modal('show');
-	$("#pseudoSubmit").click(function() {setPseudo()});
-	$("#chatEntries").slimScroll({height: '600px'});
-	submitButton.click(function() {sentMessage();});
-	setHeight();
+    messageContainer = $('#messageInput');
+    submitButton = $("#submit");
+    bindButton();
+    window.setInterval(time, 1000*10);
+    $("#alertPseudo").hide();
+    $('#modalPseudo').modal('show');
+    $("#pseudoSubmit").click(function() {setPseudo()});
+    $("#chatEntries").slimScroll({height: '600px'});
+    submitButton.click(function() {sentMessage();});
+    setHeight();
+
+    $("input").bind("keydown", function(event) {
+        // track enter key
+        var keycode = (event.keyCode ? event.keyCode : (event.which ? event.which : event.charCode));
+        if (keycode == 13) { // keycode for enter key
+            // force the 'Enter Key' to implicitly click the Send button
+            document.getElementById('submit').click();
+            return false;
+        } else  {
+            return true;
+        }
+    }); // end of function
 });
 
 //Socket.io
 var socket = io.connect();
 socket.on('connect', function() {
-	console.log('connected');
+    console.log('connected');
 });
 socket.on('nbUsers', function(msg) {
-	$("#nbUsers").html(msg.nb);
+    $("#nbUsers").html(msg.nb);
 });
 socket.on('message', function(data) {
-	addMessage(data['message'], data['pseudo'], new Date().toISOString(), false);
-	console.log(data);
+    addMessage(data['message'], data['pseudo'], new Date().toISOString(), false);
+    console.log(data);
 });
 
 //Help functions
 function sentMessage() {
-	if (messageContainer.val() != "") 
-	{
-		if (pseudo == "") 
-		{
-			$('#modalPseudo').modal('show');
-		}
-		else 
-		{
-			socket.emit('message', messageContainer.val());
-			addMessage(messageContainer.val(), "Me", new Date().toISOString(), true);
-			messageContainer.val('');
-			submitButton.button('loading');
-		}
-	}
+    if (messageContainer.val() != "") 
+    {
+        if (pseudo == "") 
+        {
+            $('#modalPseudo').modal('show');
+        }
+        else 
+        {
+            socket.emit('message', messageContainer.val());
+            addMessage(messageContainer.val(), "Me", new Date().toISOString(), true);
+            messageContainer.val('');
+            submitButton.button('loading');
+        }
+    }
 }
 function addMessage(msg, pseudo, date, self) {
-	if(self) var classDiv = "row message self";
-	else var classDiv = "row message";
-	$("#chatEntries").append('<div class="'+classDiv+'"><p class="infos"><span class="pseudo">'+pseudo+'</span>, <time class="date" title="'+date+'">'+date+'</time></p><p>' + msg + '</p></div>');
+    if(self) var classDiv = "row message self";
+    else var classDiv = "row message";
+    $("#chatEntries").append('<div class="'+classDiv+'"><p class="infos"><span class="pseudo">'+pseudo+'</span>, <time class="date" title="'+date+'">'+date+'</time></p><p>' + msg + '</p></div>');
     var elem = document.getElementById('chatEntries');
     elem.scrollTop = elem.scrollHeight;
-	time();
+    time();
 }
 
 function bindButton() {
-	submitButton.button('loading');
-	messageContainer.on('input', function() {
-		if (messageContainer.val() == "") submitButton.button('loading');
-		else submitButton.button('reset');
-	});
+    submitButton.button('loading');
+    messageContainer.on('input', function() {
+        if (messageContainer.val() == "") submitButton.button('loading');
+        else submitButton.button('reset');
+    });
 }
 function setPseudo() {
-	if ($("#pseudoInput").val() != "")
-	{
-		socket.emit('setPseudo', $("#pseudoInput").val());
-		socket.on('pseudoStatus', function(data){
-			if(data == "ok")
-			{
-				$('#modalPseudo').modal('hide');
-				$("#alertPseudo").hide();
-				pseudo = $("#pseudoInput").val();
-			}
-			else
-			{
-				$("#alertPseudo").slideDown();
-			}
-		})
-	}
+    if ($("#pseudoInput").val() != "")
+    {
+        socket.emit('setPseudo', $("#pseudoInput").val());
+        socket.on('pseudoStatus', function(data){
+            if(data == "ok")
+            {
+                $('#modalPseudo').modal('hide');
+                $("#alertPseudo").hide();
+                pseudo = $("#pseudoInput").val();
+            }
+            else
+            {
+                $("#alertPseudo").slideDown();
+            }
+        })
+    }
 }
 function time() {
-	$("time").each(function(){
-		$(this).text($.timeago($(this).attr('title')));
-	});
+    $("time").each(function(){
+        $(this).text($.timeago($(this).attr('title')));
+    });
 }
 function setHeight() {
-	$(".slimScrollDiv").height('603');
-	$(".slimScrollDiv").css('overflow', 'visible')
+    $(".slimScrollDiv").height('603');
+    $(".slimScrollDiv").css('overflow', 'visible')
 }
