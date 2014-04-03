32484c6bd83cd1c65157ce56e1f9238390ad94bf
Paul Bennett (zen) <paulbennett67@hotmail.com>
2014-04-03 21:51:40 +1100

    Joy of cloudformation

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,10 +6,10 @@ source $HOME/.nvm/nvm.sh
 nvm install v0.10.17
 nvm use v0.10.17
 
-npm install -g forever
-npm install -g mocha    
-npm install -g istanbul
-npm install
+npm install -g forever --loglevel error
+npm install -g mocha --loglevel error
+npm install -g istanbul --loglevel error
+npm install --loglevel error
 
 # Up to here we are on common ground but the next line is for running the app (non-dev)
 #PORT=8080 forever start server.js
