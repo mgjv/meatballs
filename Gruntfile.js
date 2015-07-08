95ec88ae2d2912167df5c04b33bf740ecb2a3766
Martien Verbruggen <mverbruggen@atlassian.com>
2015-07-08 15:14:07 +1000

    third travis try

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,36 +2,35 @@
 
 module.exports = function(grunt) {
 
-	// Project configuration.
-	grunt.initConfig({
-	    pkg: grunt.file.readJSON('package.json'),
-	    mocha_istanbul: {
-		    coverage: {
-		        src: 'test', // a folder works nicely
-		    }
-		},
-	    mochaTest: {
-			test: {
-				options: {
-					reporter: 'spec',
-					// captureFile: 'results.txt', // Optionally capture the reporter output to a file
-					// quiet: false, // Optionally suppress output to standard out (defaults to false)
-					// clearRequireCache: false // Optionally clear the require cache before running tests (defaults to false)
-				},
-				src: ['test/**/*.js']
-			}
-		}
-	})
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        mocha_istanbul: {
+            coverage: {
+                src: 'test', // a folder works nicely
+            }
+        },
+        mochaTest: {
+            test: {
+                options: {
+                    reporter: 'spec',
+                    // captureFile: 'results.txt', // Optionally capture the reporter output to a file
+                    // quiet: false, // Optionally suppress output to standard out (defaults to false)
+                    // clearRequireCache: false // Optionally clear the require cache before running tests (defaults to false)
+                },
+                src: ['test/**/*.js']
+            }
+        }
+    })
 
-	grunt.loadNpmTasks('grunt-mocha-test');
-	grunt.loadNpmTasks('grunt-mocha-istanbul')
+    grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-mocha-istanbul')
 
-	grunt.registerTask('coverage', ['mocha_istanbul:coverage'])
-	grunt.registerTask('test', ['mochaTest'])
-	  
-	// Default task(s).
-	grunt.registerTask('default', ['test']);
-
+    grunt.registerTask('coverage', ['mocha_istanbul:coverage'])
+    grunt.registerTask('test', ['mochaTest'])
+      
+    // Default task(s).
+    grunt.registerTask('default', ['test']);
 };
 
 
